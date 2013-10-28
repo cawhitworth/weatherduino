0b2e905f1146c7b3d0b42bc241ce997f2351f0e2
Chris Whitworth <chris.whitworth@red-gate.com>
2013-10-28 13:04:36 +0000

    Initial commit

---
+++ dump_433/dump_433.ino
@@ -0,0 +1,46 @@
+
+#define MAX_DURATIONS 256
+
+unsigned int i = 0;
+unsigned int durations[MAX_DURATIONS];
+
+const int buttonPin = 8;
+
+void setup()
+{
+  Serial.begin(9600);
+  Serial.print("Hello!");
+  attachInterrupt(0, handleInterrupt, CHANGE);
+  pinMode(buttonPin, INPUT);
+}
+
+void handleInterrupt()
+{
+   static unsigned long lastTime;
+   static unsigned int duration;
+   long time = micros();
+   duration = time - lastTime;
+   durations[i] = duration;
+   i++;
+   if (i == MAX_DURATIONS) i = 0;
+   lastTime = time;   
+}
+
+int state = LOW;
+
+void printDuration(int d)
+{
+  Serial.print(d);
+  Serial.print("  ");
+  Serial.print(durations[d]);
+  Serial.print("\n");
+}
+
+void loop()
+{
+  delay(5000);
+  Serial.print("\nTick");
+  for(int j = 0; j < i; j++)
+    printDuration(j);
+  i = 0;
+}
